7438da81e2e8042da62d3d2707fd598ead1957f5
Nicholas <nicholas.keam@gmail.com>
2013-07-16 20:12:33 -0400

    generated 2 pages, controllers, added image; ;

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  	@message = "AHHHHH"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Library::Application.routes.draw do
+  root :to => 'pages#home'
+
+  get "pages/home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
